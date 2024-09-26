da0a12126f6a3224c419ed208cd2bcd2e404e47f
mysoyul <vega2k_@naver.com>
2024-09-26 14:23:44 +0900

    Bookmark / Bookmarks 컴포넌트 추가

---
+++ src/app/components/Bookmarks.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { BookmarkType } from '@/types';
+import Bookmark from './Bookmark';
+
+type Props = {
+    bookmarks: BookmarkType[]
+}
+
+const Bookmarks: React.FC<Props> = ({bookmarks}) => {
+    return (
+        <div>
+            <h1>Welcome to Bookmarker</h1>
+            {
+            bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark} />)
+            }            
+        </div>
+    );
+};
+
+export default Bookmarks;
