81b706ed0952a113ad28ebfd101f3aa87da73121
mysoyul <vega2k_@naver.com>
2024-09-26 11:45:54 +0900

    layout.tsx update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import NavBar from "@/app/components/NavBar";
 
 export default function Home() {
   return (
     <div>
-      <NavBar />
-      <main>
         <h1>Welcome to Bookmarker</h1>
-      </main>
     </div>
   );
 }
